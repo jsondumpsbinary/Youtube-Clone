02ab574e75cfa28a028934a3803777d6c8831502
Anu Shree Birla <anushreebirla14@gmail.com>
2026-04-24 20:49:24 +0530

    Sidebar made

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import { useState } from 'react'
 import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
 import './App.css'
 
 function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
+  function handleToggleSidebar() {
+    setSidebarOpen((prev) => !prev);
+  }
+
   return (
     <div className="app">
       <Navbar onToggleSidebar={handleToggleSidebar} onGoHome={handleGoHome} />
+
+      <div className="app__body">
+
+        <Sidebar isOpen={sidebarOpen} />
+          
+      </div>
     </div>
   );
 }
