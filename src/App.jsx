a4353622c99fd658093ae2d963c3b1eb4ac5294a
Anu Shree Birla <anushreebirla14@gmail.com>
2026-04-24 20:27:10 +0530

    navbar styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import Navbar from "./components/Navbar/Navbar";
 import './App.css'
 
 function App() {
-  return(
-    <></>
-  )
+  return (
+    <div className="app">
+      <Navbar onToggleSidebar={handleToggleSidebar} onGoHome={handleGoHome} />
+    </div>
+  );
 }
 
 export default App
