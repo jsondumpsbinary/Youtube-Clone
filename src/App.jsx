83eb3c15a8a40b5f2eb6f83ab297d971f6b601fb
Anu Shree Birla <anushreebirla14@gmail.com>
2026-04-25 14:34:01 +0530

    app.jsx finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
+import { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
-import './App.css'
+import Home from "./components/Home/Home";
+import VideoDetail from "./components/VideoDetail/VideoDetail";
+import "./App.css";
 
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState(true);
 
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
   function handleToggleSidebar() {
     setSidebarOpen((prev) => !prev);
+  }
+
+  function handleSelectVideo(video) {
+    setSelectedVideo(video);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+
+  function handleGoHome() {
+    setSelectedVideo(null);
   }
 
   return (
@@ -17,10 +30,25 @@ function App() {
       <div className="app__body">
 
         <Sidebar isOpen={sidebarOpen} />
-          
+
+        <main
+          className={`app__content ${sidebarOpen ? "app__content--shifted" : "app__content--full"}`}
+        >
+          {selectedVideo === null ? (
+
+            <Home onSelectVideo={handleSelectVideo} />
+          ) : (
+
+            <VideoDetail
+              video={selectedVideo}
+              onGoHome={handleGoHome}
+            />
+          )}
+        </main>
       </div>
     </div>
   );
 }
 
-export default App
+export default App;
+
